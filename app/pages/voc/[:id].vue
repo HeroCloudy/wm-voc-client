aa24523566e23ea0453fbff659749d46f5fd414e
youyacoder <yygnb@126.com>
2025-10-19 21:33:04 +0800

    feat: 通过路由获取问卷id

---
+++ app/pages/voc/[:id].vue
@@ -0,0 +1,18 @@
+<!--
+ @name: [:id].vue
+ @description: 问卷详情
+ @author: 程序员优雅哥 youyacoder
+ @time: 2025/10/19 21:28
+-->
+<template>
+  <div class="voc-detail">
+    <div>voc detail</div>
+    <div>{{ id }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+const id = route.params.id
+</script>
+<style scoped lang="scss"></style>
